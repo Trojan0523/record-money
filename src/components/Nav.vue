c31957197512c5829ea951321adc086149947fff
Trojan0523 <1153532327@qq.com>
2020-04-29 16:08:59 +0800

    解决了svg自带fill属性,viewport进行调整

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
     <nav>
-        <router-link to="/money" class="item">
+        <router-link to="/money" class="item" active-class="selected">
             <Icon name="money"/>
             记账
         </router-link>
-        <router-link to="/labels" class="item">
+        <router-link to="/labels" class="item" active-class="selected">
             <Icon name="label"/>
             标签
         </router-link>
-        <router-link to="/statistics" class="item">
+        <router-link to="/statistics" class="item" active-class="selected">
             <Icon name="statistics"/>
             统计
         </router-link>
@@ -39,11 +39,13 @@
             align-items: center;
             display: flex;
             flex-direction: column;
+            .icon {
+                height: 32px;
+                width: 32px;
+            }
         }
-
-        .icon {
-            height: 32px;
-            width: 32px;
+        > .item.selected {
+            color: 	#7FFFAA;
         }
     }
 </style>
