cbd272715a2998fdc5e885e90c91b47f3a5bf4fd
Trojan0523 <1153532327@qq.com>
2020-05-05 21:32:30 +0800

    基本完成 Notes 组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -13,7 +13,7 @@
             <button @click="inputContent">7</button>
             <button @click="inputContent">8</button>
             <button @click="inputContent">9</button>
-            <button @click="ok" class="ok">OK</button>
+            <button class="ok">OK</button>
             <button @click="inputContent" class="zero">0</button>
             <button @click="inputContent">.</button>
         </div>
