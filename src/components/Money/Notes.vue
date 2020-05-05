69e05d12724e281707eafcc1f7912e1be86251a2
Trojan0523 <1153532327@qq.com>
2020-05-05 21:38:05 +0800

    使用v-model 代替 :value 和 @input实现双向绑定

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -3,7 +3,7 @@
         <label class="notes">
             {{value}}
             <span class="name">备注</span>
-            <input type="text" :value="value" @input="onInput" placeholder="在这里输入备注">
+            <input type="text" v-model="value" placeholder="在这里输入备注">
         </label>
     </div>
 </template>
@@ -15,10 +15,6 @@
   @Component
   export default class Notes extends Vue {
     value = '';
-    onInput(event: KeyboardEvent) {
-      const input = event.target as HTMLButtonElement;
-      this.value = input.value;
-    }
   }
 </script>
 
