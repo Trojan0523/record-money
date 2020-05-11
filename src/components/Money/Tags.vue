66f36c980dc2c4d3c76c7f66708adb70758f9821
Trojan0523 <1153532327@qq.com>
2020-05-11 15:36:08 +0800

    可以选中或取消标签

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,18 +4,31 @@
             <button>新增标签</button>
         </div>
         <ul class="current">
-            <li>衣</li>
-            <li>食</li>
-            <li>住</li>
-            <li>行</li>
+            <li v-for="tag in dataSource" :key="tag" @click="select(tag)"
+                :class="{toggle: selectedTags.indexOf(tag)>=0}">{{tag}}
+            </li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Tags'
-  };
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class Tags extends Vue {
+    @Prop() dataSource: string[] | undefined;
+    selectedTags: string[] = [];
+
+    toggle(tag: string) {
+      const index = this.selectedTags.indexOf(tag);
+      if (index >= 0) {
+        this.selectedTags.splice(index, 1);
+      } else {
+        this.selectedTags.push(tag);
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -33,7 +46,8 @@
             flex-wrap: wrap;
 
             > li {
-                background: #d9d9d9;
+                $bg: #d9d9d9;
+                background: $bg;
                 $h: 24px;
                 height: $h;
                 border-radius: $h/2;
@@ -41,6 +55,11 @@
                 margin-right: 12px;
                 line-height: $h;
                 margin-top: 4px;
+
+                &.selected {
+                    background: darken($bg, 50%);
+                    color: white;
+                }
             }
         }
 
