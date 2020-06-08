60318a9205fef1c3fc7ec9116324c62937e7e9dc
Trojan0523 <1153532327@qq.com>
2020-06-08 15:49:32 +0800

    消除Tags的警告,目前还存在bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,8 +4,8 @@
             <button @click="create">新增标签</button>
         </div>
         <ul class="current">
-            <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)"
-                :class="{selected: selectedTags.indexOf(tag)>=0}">{{tag}}
+            <li v-for="tag in dataSource" :key="tag.id" @click="toggle(tag)"
+                :class="{selected: selectedTags.indexOf(tag)>=0}">{{tag.name}}
             </li>
         </ul>
     </div>
