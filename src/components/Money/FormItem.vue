2957861cc6d9c75ff53ecb675fef870950ce5471
Trojan0523 <1153532327@qq.com>
2020-06-08 22:48:55 +0800

    FormItem 和 createId 纠错

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -5,14 +5,14 @@
             <input type="text"
                    :value="value"
                    @input="onValueChanged($event.target.value)"
-                   :placeholder="this.placeholder">
+                   :placeholder="placeholder">
         </label>
     </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class FromItem extends Vue {
@@ -21,7 +21,6 @@
     @Prop({required: true}) fieldName!: string;
     @Prop() placeholder?: string;
 
-    @Watch('value')
     onValueChanged(value: string) {
       this.$emit('update:value', value);
     }
