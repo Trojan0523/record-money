57e0cd48027595143ded84c94b810e33fc8230ce
Trojan0523 <1153532327@qq.com>
2020-06-08 22:39:51 +0800

    解决id生成问题

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagList';
 type Tag = {
   id: string;
@@ -20,9 +22,11 @@ const tagListModel: TagListModel = {
   },
   create(name: string) {
     // this.data = [{id:'1', name: '1'}, {id: '2', name: '2'}]
+
     const names = this.data.map(item => item.name);
     if (names.indexOf(name) >= 0) { return 'duplicated';}
-    this.data.push({id: name, name: name});
+    const id = createId().toString();
+    this.data.push({id, name: name});
     this.save();
     return 'success';
   },
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -46,7 +46,11 @@
     }
     remove() {
       if(this.tag) {
-        tagListModel.remove(this.tag.id);
+        if(tagListModel.remove(this.tag.id)) {
+          this.$router.back();
+        } else {
+          window.alert('删除失败');
+        }
       }
     }
     goBack() {
