f7bbcd066a033ac46aaf035239e13a496e669445
Trojan0523 <1153532327@qq.com>
2020-06-10 16:03:58 +0800

    Labels.vue 使用 Vuex, 抽离TagHelper 作为mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="tags">
         <div class="new">
-            <button @click="create">新增标签</button>
+            <button @click="createTag">新增标签</button>
         </div>
         <ul class="current">
             <li v-for="tag in tagList" :key="tag.id" @click="toggle(tag)"
@@ -12,8 +12,9 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
+  import {mixins} from 'vue-class-component';
+  import TagHelper from '@/mixins/TagHelper';
 
   @Component({
     computed: {
@@ -22,18 +23,11 @@
       }
     }
   })
-  export default class Tags extends Vue {
+  export default class Tags extends mixins(TagHelper) {
     selectedTags: string[] = [];
 
     created() {
       this.$store.commit('fetchTags');
-    }
-    create() {
-      const name = window.prompt('请输入标签名');
-      if (!name) {
-        return window.alert('标签名不能为空');
-      }
-      this.$store.commit('createTag',name);
     }
 
     toggle(tag: string) {
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -14,24 +14,25 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component,} from 'vue-property-decorator';
   import Button from '@/components/Button.vue';
+  import  { mixins } from 'vue-class-component';
+  import { TagHelper } from '@/mixins/TagHelper';
+
 
   @Component({
-    components: {Button}
-  })
-  export default class Labels extends Vue {
-    // TODO
-    tags = [];  // store.tagList;
-
-    createTag() {
-      const name = window.prompt('请输入标签');
-      if (name) {
-        // TODO
-        // store.createTag(name);
+    components: {Button},
+    computed: {
+      tags() {
+        return this.$store.state.tagList;
       }
+    },
+  })
+  export default class Labels extends mixins(TagHelper) {
+    beforeCreate() {
+      this.$store.commit('fetchTags');
     }
+    
   }
 </script>
 <style lang="scss" scoped>
