04f5b869f142c38c2ad0286c768d239c5edd70ff
Trojan0523 <1153532327@qq.com>
2020-06-14 23:46:11 +0800

    新增手机端二维码，修复dev serve下404问题

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,14 @@ new Vue({
 window.onload = function () {
   setTimeout(() => window.scrollTo(0, 200), 0);
 };
+if(document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开页面，已保证浏览效果');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
