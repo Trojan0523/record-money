bd0a3a9dd0222afab1b7c26e6d53f10a4d6e112d
Trojan0523 <1153532327@qq.com>
2020-06-14 21:06:51 +0800

    部署当前网站到 trojan-record上

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@
 const path = require('path');
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/trojan-record/'
+        : '/',
     lintOnSave: false,
     chainWebpack: config => {
         const dir = path.resolve(__dirname, 'src/assets/icons')
